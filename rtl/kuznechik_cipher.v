652b854f0a5710831852145c39c3910637dabb0d
Nikkitachka <Nikkitachka@yandex.ru>
2022-04-10 20:31:36 +0300

    описано состояние FINISH_S

--- rtl/kuznechik_cipher.v
+++ rtl/kuznechik_cipher.v
@@ -1,31 +1,31 @@
 module kuznechik_cipher(
-    input               clk_i,      // Тактовый сигнал
-                        resetn_i,   // Синхронный сигнал сброса с активным уровнем LOW
-                        request_i,  // Сигнал запроса на начало шифрования
-                        ack_i,      // Сигнал подтверждения приема зашифрованных данных
-                [127:0] data_i,     // Шифруемые данные
+  input              clk_i,      // Тактовый сигнал
+                     resetn_i,   // Синхронный сигнал сброса с активным уровнем LOW
+                     request_i,  // Сигнал запроса на начало шифрования
+                     ack_i,      // Сигнал подтверждения приема зашифрованных данных
+             [127:0] data_i,     // Шифруемые данные
 
-    output              busy_o,     // Сигнал, сообщающий о невозможности приёма
-                                    // очередного запроса на шифрование, поскольку
-                                    // модуль в процессе шифрования предыдущего
-                                    // запроса
-           reg          valid_o,    // Сигнал готовности зашифрованных данных
-           reg  [127:0] data_o      // Зашифрованные данные
+  output             busy_o,     // Сигнал, сообщающий о невозможности приёма
+                                 // очередного запроса на шифрование, поскольку
+                                 // модуль в процессе шифрования предыдущего
+                                 // запроса
+         reg         valid_o,    // Сигнал готовности зашифрованных данных
+         reg [127:0] data_o      // Зашифрованные данные
 );
 
-reg [127:0] key_mem [0:9];
+  reg [127:0] key_mem [0:9];
 
-reg [7:0] S_box_mem [0:255];
+  reg [7:0] S_box_mem [0:255];
 
-reg [7:0] L_mul_16_mem  [0:255];
-reg [7:0] L_mul_32_mem  [0:255];
-reg [7:0] L_mul_133_mem [0:255];
-reg [7:0] L_mul_148_mem [0:255];
-reg [7:0] L_mul_192_mem [0:255];
-reg [7:0] L_mul_194_mem [0:255];
-reg [7:0] L_mul_251_mem [0:255];
+  reg [7:0] L_mul_16_mem  [0:255];
+  reg [7:0] L_mul_32_mem  [0:255];
+  reg [7:0] L_mul_133_mem [0:255];
+  reg [7:0] L_mul_148_mem [0:255];
+  reg [7:0] L_mul_192_mem [0:255];
+  reg [7:0] L_mul_194_mem [0:255];
+  reg [7:0] L_mul_251_mem [0:255];
 
-initial begin
+  initial begin
     $readmemh("keys.mem",key_mem );
     $readmemh("S_box.mem",S_box_mem );
 
@@ -36,7 +36,7 @@ initial begin
     $readmemh("L_192.mem",L_mul_192_mem);
     $readmemh("L_194.mem",L_mul_194_mem);
     $readmemh("L_251.mem",L_mul_251_mem);
-end
+  end
 
   reg [3:0] round_cnt;
   reg [3:0] l_phase_cnt;
@@ -101,11 +101,10 @@ end
 
         FINISH_S:
           begin
-         //   if( ... ) 
+            if( request_i ) 
               next_state = IDLE_S;
-         //   else
-         //     if( )
-                next_state = KEY_PHASE_S; 
+            else if( ack_i )
+              next_state = KEY_PHASE_S;
           end
 
         default:
